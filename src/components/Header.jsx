529df6500b767a4a1639012c1a1a2d2b0330a0af
ADE OLAOYE <adeolaoye56@gmail.com>
2023-09-17 22:15:50 +0100

    add mobile menu component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {RiMenu2Fill} from 'react-icons/ri'
+import MobileMenu from './MobileMenu'
 
 
-const Navigation = () => {
+const Header = () => {
+    const [isMenuClicked, setIsMenuClicked] = useState(true)
+
+    const handleClick = () => {
+        setIsMenuClicked(!isMenuClicked)
+    }
+
     return (
         <div className='header-wrapper'>
             <div></div>
             <h1 className='logo'>olaDEV</h1>
-            <span  className='menu-icon'><RiMenu2Fill /></span>
+            <div className='icon-menu-wrapper'>{isMenuClicked ? <MobileMenu /> : <span  className='menu-icon' onClick={handleClick}><RiMenu2Fill /></span>}</div>
         </div>
     )
 }
 
-export default Navigation;
+export default Header;
